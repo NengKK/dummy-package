dc300df2b834badf81eb348972ce3bd9f56b8b80
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2019-12-04 10:29:05 +0700

    Fix function return value

--- src/Sftp.ts
+++ src/Sftp.ts
@@ -1,10 +1,10 @@
 import SftpConnector from './emis/sftp-connector';
 
-export async function GetFile(fileName: string) {
+export async function GetFile(fileName: string): Promise<string> {
     const sftp = new SftpConnector();
     await sftp.connect();
 
-    sftp.get(fileName)
+    return await sftp.get(fileName)
         .then(() => `Get file successful file=${fileName}`)
         .catch(err => `Get file error err=${err}`);
 }
